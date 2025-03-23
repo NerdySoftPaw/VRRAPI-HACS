c44b66d14da44a008499b6feeaaea3f25bc40e59
Kevin Kugler <git@nerdysoftpaw.de>
2025-03-23 16:44:30 +0100

    add init.py

--- custom_components/vrr-api/init.py
+++ custom_components/vrr-api/init.py
@@ -1,14 +1,13 @@
-"""Init file for VRR integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    """Set up VRR from a config entry."""
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up VRR Departures from a config entry."""
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
 
-async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    """Unload VRR config entry."""
-    return await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload VRR Departures config entry."""
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
