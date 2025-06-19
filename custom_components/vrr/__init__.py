c562e9ac619b712d5111136b7c88bd27a178d7f9
Kevin Kugler <git@nerdysoftpaw.de>
2025-06-19 11:22:32 +0000

    fix error

--- custom_components/vrr/__init__.py
+++ custom_components/vrr/__init__.py
@@ -1,4 +1,4 @@
-adfrom homeassistant.config_entries import ConfigEntry
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
