1726e25bab607c44ddf1af246f4ff13dcdaf8ed7
Kevin Kugler <git@nerdysoftpaw.de>
2025-06-21 21:50:59 +0200

    enhance VRR integration setup and unload processes, add config flow support

--- custom_components/vrr/__init__.py
+++ custom_components/vrr/__init__.py
@@ -7,14 +7,28 @@ PLATFORMS = [Platform.SENSOR]
 
 async def async_setup(hass: HomeAssistant, config):
     """Set up the VRR integration from configuration.yaml."""
+    # Diese Funktion ist für den Config Flow weniger relevant, aber wichtig
+    # wenn YAML Unterstützung parallel beibehalten werden soll
+    hass.data.setdefault(DOMAIN, {})
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up VRR Departures from a config entry."""
     hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(async_update_options))
+    return True
+
+async def async_update_options(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Update options."""
+    await hass.config_entries.async_reload(entry.entry_id)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload VRR Departures config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+    return unload_ok
