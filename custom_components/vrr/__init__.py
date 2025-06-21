d659b96237a8d71c9e9d6fd37c8cacec0f37928d
Kevin Kugler <git@nerdysoftpaw.de>
2025-06-21 21:42:19 +0200

    refactor VRR integration to support multiple platforms and improve entry setup

--- custom_components/vrr/__init__.py
+++ custom_components/vrr/__init__.py
@@ -1,11 +1,20 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
+
+DOMAIN = "vrr"
+PLATFORMS = [Platform.SENSOR]
+
+async def async_setup(hass: HomeAssistant, config):
+    """Set up the VRR integration from configuration.yaml."""
+    return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up VRR Departures from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    hass.data.setdefault(DOMAIN, {})
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload VRR Departures config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
