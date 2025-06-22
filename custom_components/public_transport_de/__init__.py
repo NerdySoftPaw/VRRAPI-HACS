e5791ccfd4ffd51943114e718075d0e92cb9e042
Kevin Kugler <git@nerdysoftpaw.de>
2025-06-22 22:21:03 +0200

    Add Public Transport DE integration and update related strings

--- custom_components/public_transport_de/__init__.py
+++ custom_components/public_transport_de/__init__.py
@@ -1,11 +1,18 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the Public Transport DE component."""
+    return True
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up VRR Departures from a config entry."""
+    """Set up Public Transport DE from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload VRR Departures config entry."""
+    """Unload Public Transport DE config entry."""
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
